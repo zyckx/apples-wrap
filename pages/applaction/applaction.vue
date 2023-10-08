d87ebc1fedf0883b050b383a3747a3465b78e4ae
“zygalaxy” <694553456@qq.com>
2023-10-08 21:24:10 +0800

    first commit

---
+++ pages/applaction/applaction.vue
@@ -0,0 +1,132 @@
+<template>
+	<Layout mode='zoom' show="show">
+		<view class="application">
+			<div class="search">
+				<u-search placeholder="日照香炉生紫烟" v-model="keyword" @search="searchSubmit"></u-search>
+			</div>
+			<div class="article-list">
+				<u-list @scrolltolower="scrolltolower">
+					<u-list-item v-for="item in articleList" :key="item.id">
+						<u-cell :title="item.title" isLink :label="item.description">
+							<template #icon>
+								<u-avatar shape="square" size="35" src="https://cdn.uviewui.com/uview/album/1.jpg"
+									customStyle="margin: -3px 5px -3px 0"></u-avatar>
+							</template>
+							<template #right-icon>
+								<u-icon name="arrow-right"></u-icon>
+							</template>
+						</u-cell>
+					</u-list-item>
+				</u-list>
+			</div>
+		</view>
+	</Layout>
+</template>
+
+<script setup>
+	import Layout from '../../components/Layout/Layout.vue'
+	import BackTop from '../../components/BackTop/BackTop.vue'
+
+	import {
+		onLoad,
+		onPageScroll
+	} from '@dcloudio/uni-app'
+	onLoad(() => {})
+	onPageScroll(() => {})
+	import {
+		reactive,
+		ref
+	} from "vue";
+
+	const keyword = ref(null)
+	const show = ref(true)
+	const scrollTopStyle = reactive({
+		iconStyle: {
+			fontSize: '32rpx',
+			color: '#2979ff'
+		}
+	})
+	const searchSubmit = (e) => {
+		console.log(e)
+	}
+	const articleList = ref([{
+		id: 1,
+		title: "我是测试标题啊",
+		description: "我是介绍啊"
+	}, {
+		id: 2,
+		title: "我是测试标题啊",
+		description: "我是介绍啊"
+	}, {
+		id: 3,
+		title: "我是测试标题啊",
+		description: "我是介绍啊"
+	}, {
+		id: 4,
+		title: "我是测试标题啊",
+		description: "我是介绍啊"
+	}, {
+		id: 5,
+		title: "我是测试标题啊",
+		description: "我是介绍啊"
+	}, {
+		id: 6,
+		title: "我是测试标题啊",
+		description: "我是介绍啊"
+	}, {
+		id: 7,
+		title: "我是测试标题啊",
+		description: "我是介绍啊"
+	}, {
+		id: 8,
+		title: "我是测试标题啊",
+		description: "我是介绍啊"
+	}, {
+		id: 9,
+		title: "我是测试标题啊",
+		description: "我是介绍啊"
+	}, {
+		id: 10,
+		title: "我是测试标题啊",
+		description: "我是介绍啊"
+	}, {
+		id: 11,
+		title: "我是测试标题啊",
+		description: "我是介绍啊"
+	}, {
+		id: 12,
+		title: "我是测试标题啊",
+		description: "我是介绍啊"
+	}, {
+		id: 13,
+		title: "我是测试标题啊",
+		description: "我是介绍啊"
+	}, {
+		id: 14,
+		title: "我是测试标题啊",
+		description: "我是介绍啊"
+	}, {
+		id: 15,
+		title: "我是测试标题15啊",
+		description: "我是介绍啊"
+	}, {
+		id: 16,
+		title: "我是测试标题16啊",
+		description: "我是介绍啊"
+	}])
+	const scrolltolower = () => {
+		console.log('触底了')
+	}
+</script>
+
+<style lang="scss">
+	.application {
+		position: relative;
+		width: 100%;
+		min-height: 100%;
+
+		.search {
+			padding: 27rpx 17rpx;
+		}
+	}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,109 @@
+<template>
+	<Layout mode='zoom' show="show">
+		<view class="content">
+			<image class="logo" src="/static/logo.png"></image>
+			<view class="text-area">
+				<u-alert :title="title" type="primary"></u-alert>
+			</view>
+			<view class="identify-box">
+				<view class="take-photo" @click="takePhoto">
+				</view>
+				<view class="upload-image" @click="chooseImage">
+				</view>
+			</view>
+		</view>
+	</Layout>
+</template>
+
+<script setup>
+	import {
+		ref
+	} from "vue";
+	import Layout from '../../components/Layout/Layout.vue'
+	const title = ref('欢迎使用苹果套袋识别系统')
+
+	const takePhoto = () => {
+		uni.chooseImage({
+			count: 1, //选择一张
+			sizeType: ['original', 'compressed'], //可以指定是原图还是压缩图，默认二者都有
+			sourceType: ['camera'], //从相册选择
+			success: function(res) {
+				console.log(JSON.stringify(res.tempFilePaths));
+			}
+		});
+	}
+	const chooseImage = () => {
+		console.log("choose image")
+		uni.chooseImage({
+			count: 1, //选择一张
+			sizeType: ['original', 'compressed'], //可以指定是原图还是压缩图，默认二者都有
+			sourceType: ['album'], //从相册选择
+			success: function(res) {
+				console.log(JSON.stringify(res.tempFilePaths));
+			}
+		});
+	}
+	const uploadImage = () => {
+		console.log("upload image")
+	}
+	const identify = () => {
+		console.log("identify.......")
+	}
+</script>
+
+<style lang="scss">
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+
+		.logo {
+			height: 200rpx;
+			width: 200rpx;
+			margin-top: 200rpx;
+			margin-left: auto;
+			margin-right: auto;
+			margin-bottom: 50rpx;
+		}
+
+		.text-area {
+			display: flex;
+			justify-content: space-between;
+			flex-direction: column;
+
+			.title {
+				font-size: 36rpx;
+				color: #8f8f94;
+			}
+		}
+
+		.identify-box {
+			width: 100%;
+			margin-top: 120upx;
+			min-height: 400upx;
+			display: flex;
+			justify-content: space-around;
+			// align-items: center;
+
+			.take-photo,
+			.upload-image {
+				width: 200upx;
+				height: 200upx;
+				background-color: #8f8f94;
+			}
+
+			.take-photo {
+				background: url("../../static/images/拍照.png")no-repeat;
+				background-size: cover;
+				background-position: 0 0;
+			}
+
+			.upload-image {
+				background: url("../../static/images/上传.png")no-repeat;
+				background-size: cover;
+				background-position: 0 0;
+			}
+		}
+	}
+</style>
